72cc36e356771331f2c9b21912c069abb93935f8
Tomasz Gręda <tomaszgreda@live.com>
2020-02-29 13:07:47 +0100

    2

--- Bazy danych/pierwsza_baza.sql
+++ Bazy danych/pierwsza_baza.sql
@@ -1,14 +1,12 @@
-#1. Tworzenie bazy danych i użytkownika 
+
 CREATE DATABASE library;
 USE library;
 CREATE USER 'library_user'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON library.* TO 'library_user'@'localhost';
 FLUSH PRIVILEGES;
 
-# Odbieranie uprawnień użytkownikom 
-# REVOKE ALL PRIVILEGES ON library.* FROM 'library_user'@'localhost';
 
-#2. tworzenie tabel, komenda if not exiist - spraawdza czy istnieje juz tabela 
+
 create table if not exists author (
 first_name varchar(128),
 last_name varchar(128)
@@ -27,5 +25,12 @@ create table if not exists book (
  show databases;
  show tables;
  desc book;
+insert into book ( title, author, published, isbn, category, page_count, publisher, price)
+values('Spring w akcji, Wydanie IV', 'Craig Walls', '2015-08-13', '978-83-283-0849-7', 'Programowanie Java' 
+, 624, 'Helion', 89.00);
 
+insert into book ( title, author, published, isbn, category, page_count, publisher, price)
+values('MySQL. Vademecum profesjonalisty.', 'Paul DuBios', '2014-03-28', '978-83-246-8146-4', 'Bazy danych',
+1216, 'Helion', 149.90);
+select* from book;
 
